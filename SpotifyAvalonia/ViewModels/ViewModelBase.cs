9e63d40a886c2252e5a200468ef1b2725048743f
Indigo Hartsell <indiharts@proton.me>
2024-11-03 17:36:35 -0500

    mvvm

---
+++ SpotifyAvalonia/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace SpotifyAvalonia.ViewModels;
+
+public class ViewModelBase : ObservableObject
+{
+}
---
+++ SpotifyAvalonia/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace SpotifyAvalonia.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
